60663cc35fa56d1f4a06dc3552f8655528169660
renupatel <renuka112016@gmail.com>
2018-07-06 21:09:16 -0400

    Index.html changed

--- src/main/java/com/springbootexample/web/controllers/IndexController.java
+++ src/main/java/com/springbootexample/web/controllers/IndexController.java
@@ -1,15 +1,13 @@
-/*
-package com.controllers;
+package com.springbootexample.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Helloworld {
+public class IndexController {
 
     @RequestMapping("/")
-    public String loadIndex(){
-        return "index";
+    public String home(){
+        return "index" ;
     }
 }
-*/
